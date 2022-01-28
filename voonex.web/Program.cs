392495515563b9b844a0c56d8d8323149e1ddbe0
Alex <69281817+juuuuce@users.noreply.github.com>
2022-01-28 20:35:57 +0000

    set up fluent routing

--- voonex.web/Program.cs
+++ voonex.web/Program.cs
@@ -1,11 +1,24 @@
+using FluentBlazorRouter;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using voonex.web;
+using voonex.web.Pages;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluentRouting<voonex.web.Pages.Index>(rootBuilder =>
+{
+    rootBuilder.WithPage<Counter>("meow", meowBuilder =>
+    {
+        meowBuilder.WithGroup("meowmeow", groupBuilder =>
+        {
+            groupBuilder.WithPage<FetchData>("meowmeow");
+        });
+    });
+});
 
 await builder.Build().RunAsync();
