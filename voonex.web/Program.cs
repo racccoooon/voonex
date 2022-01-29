09cc268e131ccedbb5397fb19375c792fa3bd25b
the127 <julian.baehr@googlemail.com>
2022-01-29 17:09:44 +0100

    create actual pages

--- voonex.web/Program.cs
+++ voonex.web/Program.cs
@@ -12,14 +12,12 @@ builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddFluentRouting<voonex.web.Pages.Index>(rootBuilder =>
+builder.Services.AddFluentRouting(rootBuilder =>
 {
-    rootBuilder.WithPage<Counter>("meow", meowBuilder =>
+    rootBuilder.WithPage<Dashboard>("dashboard");
+    rootBuilder.WithGroup("server", serverBuilder =>
     {
-        meowBuilder.WithGroup("meowmeow", groupBuilder =>
-        {
-            groupBuilder.WithPage<FetchData>("meowmeow");
-        });
+        serverBuilder.WithPage<CreateServer>("create");
     });
 });
 
