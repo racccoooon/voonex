66d0806372aa21c8c7f0602af87ac76af7974479
the127 <julian.baehr@googlemail.com>
2022-01-30 21:28:33 +0100

    implement login controller

---
+++ voonex.web/Services/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace voonex.web.Services;
+
+public class LoginManager : ILoginManager
+{
+    private readonly Func<Task> _refreshApp;
+
+    public LoginManager(Func<Task> refreshApp)
+    {
+        _refreshApp = refreshApp;
+    }
+
+    public async Task RefreshLoginStateAsync()
+    {
+        await _refreshApp.Invoke();
+    }
+}
+
+public interface ILoginManager
+{
+    Task RefreshLoginStateAsync();
+}
